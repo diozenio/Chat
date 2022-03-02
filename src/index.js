2b29680391eaf98e1c512b46a24fb950c6e90817
diozenio <d.dionisiobsl@gmail.com>
2022-03-02 13:28:35 -0300

    Separacao de pastas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/Login/");
+});
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html");
-})
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/Register/");
+});
 
-app.get('/register', (req, res)=>{
-    res.sendFile(__dirname + "/views/register.html");
-})
+require("./controllers/register")(app);
+require("./controllers/login")(app);
 
-
-require('./controllers/register')(app);
-require('./controllers/login')(app);
-
-app.listen(3000, ()=> {
-    console.log("Servidor sendo executado em: http://localhost:3000/");
+http.listen(3000, () => {
+  console.log("Servidor sendo executado em: http://localhost:3000/");
 });
