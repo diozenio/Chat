8057d951b14d348ef9537320d1431c40bdb216ca
diozenio <d.dionisiobsl@gmail.com>
2022-03-02 01:49:01 -0300

    Register e Login

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 
-// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -10,11 +9,13 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/views/index.html");
 })
 
-app.post('/', (req, res)=>{
-    res.send("Funcionou")
+app.get('/register', (req, res)=>{
+    res.sendFile(__dirname + "/views/register.html");
 })
 
-require('./controllers/authController')(app);
+
+require('./controllers/register')(app);
+require('./controllers/login')(app);
 
 app.listen(3000, ()=> {
     console.log("Servidor sendo executado em: http://localhost:3000/");
