f091c0f60669336c3ca0df6e84b2e36efed03450
diozenio <d.dionisiobsl@gmail.com>
2022-03-03 21:18:45 -0300

    Titulo login e register

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
 
 let port = process.env.PORT || 3000;
 
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
 app.set("views", "./views");
 app.set("view engine", "ejs");
-app.use(cookieParser());
-
-  
 app.use(express.static("public"));
+  
 
 app.get("/", (req, res) => {
   res.render(__dirname + "/views/index.ejs");
